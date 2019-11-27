c73b03db9021e7fa9a247df740ce76a40b45ff6a
Muhammad Shamsul Maruf <smaruf00ruet320@gmail.com>
2019-11-27 18:02:03 +0600

    Recursive combination 2019.11.27.1759

---
+++ src/java/Combination.java
@@ -0,0 +1,22 @@
+public clase Combination {
+  // refer: https://www.baeldung.com/java-combinations-algorithm
+  // recursive: C(n, r) = C(n-1, r-1) + C(n-1, r)
+  
+  private void helper(List<int[]> combinations, int data[], int start, int end, int index) {
+    if (index == data.length) {
+        int[] combination = data.clone();
+        combinations.add(combination);
+    } else if (start <= end) {
+        data[index] = start;
+        helper(combinations, data, start + 1, end, index + 1);
+        helper(combinations, data, start + 1, end, index);
+    }
+  }
+  
+  public List<int[]> generate(int n, int r) {
+    List<int[]> combinations = new ArrayList<>();
+    helper(combinations, new int[r], 0, n-1, 0);
+    return combinations;
+  }
+  
+}
