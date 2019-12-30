3b1499f0d74940eba312942110a3aeff60fc14d1
Muhammad Shamsul Maruf <smaruf00ruet320@gmail.com>
2019-12-30 17:33:09 +0600

    Create Spliter 2019.12.30.1732

---
+++ src/java/Spliter.java
@@ -0,0 +1,10 @@
+import java.util.Spliterators
+
+
+public class Spliter {
+  public static void main(String[] args) {
+      Spliterators.spliterator(new Integer[]{14, 15, 26, 33, 39}, 4);
+  
+  }
+
+}
