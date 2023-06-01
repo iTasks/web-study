c75be262f5cd8f300c5d6b1a91cea9bbd4b9441e
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2023-06-01 13:10:26 +0200

    Create stepfunction_rest_api_stack.ts 230601-1310

---
+++ src/react-native/stepfunction_rest_api_stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+export class StepfunctionsRestApiStack extends cdk.Stack {
+    constructor(app: cdk.App, id: string) {
+      super(app, id);
+
+      const startState = new stepfunctions.Pass(this, 'PassState', {
+          result: { value:'Hello back to you!' },
+      })
+
+      const stateMachine = new stepfunctions.StateMachine(this, 'CDKStateMachine', {
+          definition: startState,
+          stateMachineType: stepfunctions.StateMachineType.EXPRESS,
+      });
+
+      const api = new apigateway.StepFunctionsRestApi(this, 'CDKStepFunctionsRestApi', { stateMachine: stateMachine });
+    }
+}
