6fac5cce4ef30a52fb9f208f112eac246f74037f
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2023-06-01 13:11:44 +0200

    Create stepfunction_rest_api.ts 230601-1311

---
+++ src/react-native/stepfunction_rest_api.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { StepfunctionsRestApiStack } from '../lib/stepfunctions-rest-api-stack';
+
+const app = new cdk.App();
+new StepfunctionsRestApiStack(app, 'CDKStepfunctionsRestApiStack');
